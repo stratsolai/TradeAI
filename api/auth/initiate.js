d904e6b4113eb231d42b979480bc10aa3bfdaf04
Natalie Mitchell <natalie@stratsol.net.au>
2026-04-08 14:50:21 +1000

    Remove dead google-drive entry from initiate.js

--- api/auth/initiate.js
+++ api/auth/initiate.js
@@ -13,12 +13,6 @@ module.exports = async (req, res) => {
       scopes: 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/userinfo.email',
       redirectUri: APP_BASE_URL + '/api/auth/google/callback'
     },
-    'google-drive': {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      authUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
-      scopes: 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/userinfo.email',
-      redirectUri: APP_BASE_URL + '/api/auth/google-drive/callback'
-    },
     gmail: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       authUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
